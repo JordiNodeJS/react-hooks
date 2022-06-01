af1825ab774da017f535f8e4f0ddef5ef68b7ac2
JordiNodeJS <71975008+JordiNodeJS@users.noreply.github.com>
2022-06-01 12:58:41 +0200

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css"
 import RealExampleRef from "./components/useRef/RealExampleRef"
-
+import "./components/reducer/reducer"
 export default function App() {
   return (
     <>
---
+++ src/components/reducer/reducer.js
@@ -0,0 +1,35 @@
+const initialState = [
+  {
+    id: 1,
+    todo: 'to do the shopping',
+    done: false,
+  },
+]
+
+const todoReducer = (state = initialState, action) => {
+
+    action?.type === 'add' ?  [...state, action.payload] :  state
+
+//   if (action?.type === 'add') {
+//     return [...state, action.payload]
+//   }
+//   return state
+
+}
+
+const todo_1 = {
+  id: 2,
+  todo: 'to do the whashing machine',
+  done: true,
+}
+
+const addAction = {
+  type: 'add',
+  payload: todo_1,
+}
+
+let todos = todoReducer()
+
+todos = todoReducer(todos, addAction)
+
+console.log(todos)
